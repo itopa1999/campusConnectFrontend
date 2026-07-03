ff7af7c68713ffe6eb1105c78fb07b7446d93ae0
Lucky <salawulucky08071@gmail.com>
2026-07-04 00:54:26 +0100

    commiting

--- assets/js/main.js
+++ assets/js/main.js
@@ -931,6 +931,13 @@ async function refreshPointBalance() {
   }
 }
 
+function initTooltips() {
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+}
+
 // ========== START EVERYTHING AFTER DOM READY ==========
 function initAll() {
   injectNavigation();
@@ -939,6 +946,7 @@ function initAll() {
   initMobileMenu();
   initMobileMenuOutsideClick();
   initReportLostFeature();
+  initTooltips();
 }
 
 if (document.readyState === 'loading') {
